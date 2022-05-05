6560998064e9eebb56a147cd5967f5b10b3ba77e
Youssef Restom <youssefrestom@gmail.com>
2022-05-05 12:24:43 +0200

    feat: Retry for Schedule Request

--- scheduled_api/scheduled_api/doctype/schedule_request/schedule_request.js
+++ scheduled_api/scheduled_api/doctype/schedule_request/schedule_request.js
@@ -2,7 +2,21 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Schedule Request', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function (frm) {
+		if (!frm.is_new() && !frm.is_dirty()) {
+			frm.add_custom_button('Retry', () => {
+				frappe.call({
+					method: 'scheduled_api.process.execute',
+					args: {
+						kwargs: frm.doc.name
+					},
+					callback: function (r) {
+						if (!r.exc) {
+							frm.reload_doc();
+						}
+					}
+				});
+			});
+		}
+	}
 });
